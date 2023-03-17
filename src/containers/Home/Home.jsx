2bdcaafeae5babd34455bd4f0cab3ca798f1e0e4
Leandro Silva <leeansilva@gmail.com>
2023-03-17 13:44:36 -0300

    ultimos detalles a home

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,36 +1,32 @@
 import React, { useState } from 'react'
-
 import './style.css'
 import 'animate.css';
-
 import GoogleLog from '../../components/login/Google/GoogleLog';
 import { UseDataContext } from '../../context/dataContext';
-
-import { useNavigate } from 'react-router-dom';
 
 //npm run dev -- --host
 
 const Home = () => {
+  const { user, isLoading} = UseDataContext();
 
-  //dashboard
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-
-  const { user} = UseDataContext();
-  
   return (
     <>
     {
-   
-        <div className={user?.displayName ? ' container_title'  : 'container_title'}>
+        !isLoading && !user?.displayName ? 
+        <div className={user?.displayName ? ' container_title animate__animated animate__backOutDown'   : 'container_title'}>
           <h1 className='animate__animated animate__bounce text'>
               JUST PLAY.
           </h1>
           <GoogleLog/>
         </div>
-}
-
-    
+        :
+        <div className={user?.displayName ? ' container_title animate__animated animate__bounceInDown'   : 'container_title'}>
+          <h1 className='animate__animated animate__bounce text'>
+              Welcome {user.displayName}
+          </h1>
+          <GoogleLog/>
+        </div>
+    }
     </>
   )
 }
